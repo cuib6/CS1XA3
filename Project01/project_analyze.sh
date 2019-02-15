90456bceb8df8216a33915eb6a6f1343318930f8
cuib6 <cuib6@mcmaster.ca>
2019-02-15 23:41:46 +0000

    filling out README.md

--- Project01/project_analyze.sh
+++ Project01/project_analyze.sh
@@ -16,3 +16,13 @@ if [[ "$1" == "6" ]]
 		echo "No parameter is given!"
 		echo "Enter scrpt number after .sh"
 fi
+
+
+
+
+i did the first and the sixth script. for the first step, i used an IF function to choose which script to use.
+and for the second one which is the delete temporary files, i use the given code combined with if function, to test if there is a .tmp files in the repo.
+If it is find then just delete it.
+
+
+
