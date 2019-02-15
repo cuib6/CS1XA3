01a338cede4bf2a1565514250e053cb6d006caf9
cuib6 <cuib6@mcmaster.ca>
2019-02-15 23:44:01 +0000

    filling README.md

--- Project01/project_analyze.sh
+++ Project01/project_analyze.sh
@@ -20,9 +20,7 @@ fi
 
 
 
-i did the first and the sixth script. for the first step, i used an IF function to choose which script to use.
-and for the second one which is the delete temporary files, i use the given code combined with if function, to test if there is a .tmp files in the repo.
-If it is find then just delete it.
+
 
 
 
