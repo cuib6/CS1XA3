546357f9040efd4c5bcdf7a9daa47af74e988d86
cuib6 <cuib6@mcmaster.ca>
2019-02-26 22:54:46 +0000

    custom feature and number2

--- Project01/README.md
+++ Project01/README.md
@@ -2,3 +2,12 @@
 #i did the first and the sixth script. for the first step, i used an IF function to choose which script to use.
 and for the second one which is the delete temporary files, i use the given code combined with if function, to test if there is a .tmp files in the repo.
 If it is find then just delete it.
+
+
+
+when u type 2~8 after project_analyze.sh, there will be several functions:
+when u type project_analyze.sh 6, it will display deleting temporary files and will delete files
+end with .tmp
+when u type project_analyze.sh 4, it will excute the merge log programme.
+when u type project_analyze.sh 8, it will require your ID and Password, and the id and the password will be given 
+after u enter the wrong id-password set.
--- Project01/project_analyze.sh
+++ Project01/project_analyze.sh
@@ -17,6 +17,65 @@ if [[ "$1" == "6" ]]
 		echo "Enter scrpt number after .sh"
 fi
 
+if [[ "$1" == "8" ]]
+	then 
+		echo " #8 Loginning Into a New World"
+		echo -n "ID?" 
+		read name
+		echo -n "Password?"
+		read -s passwd
+	if [ $name = "Curtis" -a $passwd = "notaprofcurtis" ];then
+			echo ""
+			echo ""
+			echo ""
+			echo ""
+			echo ""
+			echo ""
+			echo "WELCOME TO A NEW WORLD CURTIS"
+			echo ""
+			echo ""
+			echo ""
+			echo ""
+			echo ""
+			echo ""
+			else "ID or password is wrong"
+			echo "ID is Curtis"
+			echo "Password is Curtis's name on git hub"
+	
+	fi
+fi
+
+if [[ "$1" == "2" ]]
+	then
+		echo "Creating a TODO Log"
+		[ -e todo.log ] && rm todo.log
+		grep "#TODO" ../ -n -r --exclude=todo.log > ~/CS1XA3/Project01/todo.log
+
+fi
+
+if [[ "$1" == "4" ]]
+	then
+		echo "Merging Log"
+		rm -f "merge.log"
+		log=`git log --oneline`
+		while read -r line
+			do
+				if [[ ! -z `echo $line | grep "Merge"` ]] 
+					then
+						target=`echo $line | cut -d " " -f 1`
+						echo $target >> merge.log 
+						echo "$target found"
+				fi
+		done <<< $log
+		echo "Complete!"
+fi
+		
+
+
+
+
+
+
 
 
 
