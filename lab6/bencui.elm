3cd6ab7540904f63bb280a5845bf265501c012ea
cuib6 <cuib6@mcmaster.ca>
2019-03-03 05:21:40 +0000

    second update

--- lab6/bencui.elm
+++ lab6/bencui.elm
@@ -1,33 +1,34 @@
 module Main exposing (main)
+
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput)
 
 main =
-		Browser.sandbow { init = init, update = update, view = view }
+      Browser.sandbox { init = init, update = update, view = view }
 
 type alias Model =
-		{ first : String, second : String }
+      { alpha : String, beta : String }
 
 init : Model
 init = 
-		Model "" ""
+       Model "" ""
 
 type Msg
-		= First String
-		| Second String
+        = Alpha String
+        | Beta String
 
-update : Msg->Model->
+update : Msg -> Model -> Model
 update msg model = 
-		case msg of 
-				First first ->
-						{ model | first = first }
-				Second second ->
-						{ model | second = second }
-view : Mdoel->Html Msg
+        case msg of 
+                Alpha alpha ->
+                        { model | alpha = alpha }
+                Beta beta ->
+                        { model | beta = beta }
+view : Model -> Html Msg
 view model = 
-		div [] 
-				[ input [ type_ "text", value model.first, onInputFirst ] 
-				[], input [ type_ "text", value model.second, onInput Second ]
-				[], div [] [ test (model.first), text ":", text (model.second) ] ]
+        div [] 
+                [ input [ type_ "text", value model.alpha, onInput Alpha ]  
+                [], input [ type_ "text", value model.beta, onInput Beta ] 
+                [], div [] [ text (model.alpha), text ":", text (model.beta) ] ]
